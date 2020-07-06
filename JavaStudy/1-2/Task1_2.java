1d9b60ec73fedb4b123676515a59cae4498c79e6
sotaushio <sota.baske1086@outlook.jp>
2020-07-06 19:02:22 +0900

    課題提出テスト
    1-0のコミット
    1-2の修正

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -13,7 +13,7 @@ public class Task1_2 {
         // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
         String[] country = {"JAPAN","AMERICA","KOREA"};
         // ② ①で作成した配列の要素数を出力してください。
-        System.out.println("配列countryの要素数…" + country.length);
+        System.out.println(country.length);
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
          *   1番目（先頭）の要素に 「りんご」 を代入
          *   2番目の要素に 「もも」 を代入
@@ -21,7 +21,7 @@ public class Task1_2 {
          */
         String[] strArray = {"りんご","もも","ぶどう"};
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
-        System.out.println("strArray[2]…" + strArray[1]);
+        System.out.println(strArray[1]);
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
          *  [「10」、「20」、「30」、「40」、「50」を要素の値（初期値）とする配列 intArreyの作成]
          */
